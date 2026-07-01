fb3be83a322efc9561ebca825b941027c8279eb0
Neto <boxmail1983@gmail.com>
2026-07-01 21:26:56 +0300

    add function myip

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('YD_TOKEN')
-print(token)
+
+class IpDetector()
+    def __init__(self):
+        pass
+
+    def myIP(self):
+        response = requests.get('https://api.ipify.org/',
+                                params={'format':'json'})
+        if response.status_code == 200:
+            ip = response.json()['ip']
+            return ip
+        else:
+            print('Что-то пошло не так')
